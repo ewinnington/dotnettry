2aa25189e0ae6f3db7338327ebe0793791285a4f
Eric winnington <eric.in.the.net@gmail.com>
2019-05-29 02:11:52 +0200

    Initial commit of dotnet-try repository. Requires .net core 3 preview.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace trydotnetSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region methods
+			var name = "Rain";
+			Console.WriteLine($"Hello{name.ToUpper()}!");  
+            #endregion
+        }
+    }
+}
---
+++ Program.md
@@ -0,0 +1,6 @@
+Testing the Program capabilites 
+
+``` cs --region methods --source-file .\Program.cs --project .\try.csproj 
+var name ="Rain";
+Console.WriteLine($"Hello {name.ToUpper()}!");
+```
